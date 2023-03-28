5da7484dcc376b4e893009689d17b90c6cf6085c
pandora <imcathalat@protonmail.com>
2023-03-28 17:05:23 -0300

    reparos-navbar

--- resources/views/components/nav-bar-component.blade.php
+++ resources/views/components/nav-bar-component.blade.php
@@ -31,7 +31,7 @@
         </div>
     </nav>
 -->
-
+<!--
 <nav class="navbar navbar-expand-md fixed-top" id="navbar">
       <div class="container fluid p-0 responsive">
         <a class="navbar-brand" href="#home">
@@ -73,4 +73,67 @@
         </div>
       </div>
     </nav>
-    
+-->
+
+
+<nav class="navbar navbar-expand-md fixed-top" id="navbar">
+      <div class="container fluid p-0 responsive">
+        <a class="navbar-brand" href="#home">
+          <img src="{{ asset('img/logo-nav.png') }}" alt= ""/>
+          <span>Mogno</span>
+        </a>
+        <button
+        class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" 
+        aria-label="Toggle navigation"
+        >
+          <i class="bi bi-list"></i>
+        </button>
+        <div class="collapse navbar-collapse text-end" id="navbar-items">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 pl-2">
+            <li class="nav-item">
+              <a class="nav-link primary-color active" aria-current="page" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link primary-color" href="#services">Serviços</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link primary-color" href="#time">Responsáveis</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link primary-color" href="/Know-How">Know How</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link primary-color" href="#duvidas">Dúvidas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link primary-color" href="#contato">Contato</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+<!--
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar w/ text</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Navbar text with an inline element
+    </span>
+  </div>
+</nav>
+-->
